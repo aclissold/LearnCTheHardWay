3eceeb392c137cf52bbeba4a129d5c483df55b82
Andrew Clissold <andrewclissold@gmail.com>
2013-06-08 13:22:21 -0400

    Did extra credit

--- ex1.c
+++ ex1.c
@@ -3,5 +3,8 @@
 int main(int argc, char *argv[])
 {
 	puts("Hello, world!");
+	puts("Here's line number two.");
+	puts("Hello, line number three!");
+	puts("I wonder if \"puts\" will\nlet me use \"\\n\"?");
 	return 0;
 }
